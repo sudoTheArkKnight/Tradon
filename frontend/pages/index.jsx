e003abae63932d7dff2a35a01086be1f8a71c888
Ayush khatri <ayushkhatri362@gmail.com>
2024-04-14 18:11:31 +0530

    Added the frontend and backend code

---
+++ frontend/pages/index.jsx
@@ -0,0 +1,11 @@
+import { HeroSection } from "@/sections/hero";
+import Layout from "../components/layout";
+import React from "react";
+
+export default function Home() {
+    return (
+        <Layout>
+            <HeroSection />
+        </Layout>
+    );
+}
