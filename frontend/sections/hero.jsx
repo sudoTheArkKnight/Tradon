63e311dc3e5dab6ed39b02506859c5a591f9df65
Ayush khatri <ayushkhatri362@gmail.com>
2024-04-24 12:20:25 +0530

    solved the conflict that happened due to not pullint the data

--- frontend/sections/hero.jsx
+++ frontend/sections/hero.jsx
@@ -2,11 +2,31 @@ import React from "react";
 
 export const HeroSection = () => {
     return (
-        <section>
-            <h1>Section 1</h1>
-            <button>
-                <a href="">logout</a>
-            </button>
+        <section className="h-screen bgImg">
+            <div className="container mx-auto flex h-screen justify-around items-center">
+                <div>
+                    <h1 className="text-white text-7xl vidaloka-regular">
+                        TRADEON
+                    </h1>
+                </div>
+                <div>
+                    <div className="flex flex-col gap-5 justify-center items-center py-28 text-white rounded-2xl px-40 blue-bg">
+                        <h1 className="mb-10 text-4xl">Welcome!</h1>
+                        <a
+                            href="/login"
+                            className="py-4 w-80 bg-blue-600  text-center hover:bg-blue-300 hover:text-black transition-all duration-200 rounded-xl"
+                        >
+                            Login
+                        </a>
+                        <a
+                            href="/signup"
+                            className="py-4 w-80 bg-blue-600 text-center hover:bg-blue-300 hover:text-black transition-all duration-200 rounded-xl"
+                        >
+                            Sign up
+                        </a>
+                    </div>
+                </div>
+            </div>
         </section>
     );
 };
