e821abddd04e6833c050551d99142f8d7d0f9dac
Ayush khatri <ayushkhatri362@gmail.com>
2024-05-12 17:00:54 +0530

    Added some code

--- frontend/src/screens/ShareDetails.jsx
+++ frontend/src/screens/ShareDetails.jsx
@@ -10,23 +10,67 @@ import {
 } from "recharts";
 import { useParams } from "react-router-dom";
 import sharedata from "../../stockData";
+import newsData from "../../newsData";
 export default function ShareData() {
     const params = useParams();
     const selectedShare = sharedata.find((s) => s.id === params.id);
     return (
-        <>
-            <div>
-                <img src={selectedShare.image} alt={selectedShare.shareName} />
-                <span>{selectedShare.high}</span>
+        <section className="bgImg shareDetailsSection">
+            <div className="sdmaindiv">
+                <div className="sdsubdiv">
+                    <div className="shareDetailsDiv">
+                        <div className="sdd2">
+                            <img
+                                src={`../../public/${selectedShare.image}`}
+                                alt={selectedShare.shareName}
+                                className="imagelogo"
+                            />
+                            <span>{selectedShare.shareName}</span>
+                            <span>₹ {selectedShare.high}</span>
+                        </div>
+                    </div>
+                    <LineChart
+                        width={800}
+                        height={450}
+                        data={selectedShare.data}
+                    >
+                        {/* <CartesianGrid strokeDasharray="5 5" /> */}
+                        <XAxis dataKey="date" />
+                        <YAxis />
+                        <Tooltip />
+                        <Legend />
+                        <Line
+                            type="monotone"
+                            dataKey="price"
+                            stroke="#008000"
+                            strokeWidth={5}
+                        />
+                    </LineChart>
+                    <div className="selectedShareDescription">
+                        {selectedShare.description}
+                    </div>
+                </div>
+                <div className="sdnews">
+                    <div>
+                        {selectedShare.gainLoss == 0 ? (
+                            <img src="../../public/up.svg" alt="Image 1" />
+                        ) : (
+                            <img
+                                src="../../public/down.jpg"
+                                alt="Image 2"
+                                className="down"
+                            />
+                        )}
+                    </div>
+                    <h2>News</h2>
+                    {newsData.map((data, index) => (
+                        <NewsCard key={index} {...data} />
+                    ))}
+                </div>
             </div>
-            <LineChart width={800} height={450} data={selectedShare.data}>
-                <CartesianGrid strokeDasharray="5 5" />
-                <XAxis dataKey="date" />
-                <YAxis />
-                <Tooltip />
-                <Legend />
-                <Line type="monotone" dataKey="price" stroke="#008000" />
-            </LineChart>
-        </>
+        </section>
     );
 }
+const NewsCard = ({ data }) => {
+    return <div className="ncard sdnewsCard">{data}</div>;
+};
